e78ca6be93763a4dda3701134c4b213cdb9a6ff1
Jordan Labonne <SilentDuskStudios@hotmail.com>
2019-09-02 19:36:09 +1000

    Allowed the developer to modify the time scale
    The developer will now be able to modify the time scale via the numpad.
    
    This will help with having to wait for actions to be called by speeding up the time.

---
+++ Project Skylit/Assets/Internal/Scripts/Helpers/ModifyTimeScale.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifyTimeScale : MonoBehaviour
+{
+    #region " - - - - - - Fields - - - - - - "
+
+
+
+    #endregion //Fields
+
+    #region " - - - - - - Methods - - - - - - "
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+            Time.timeScale = 1f;
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+            Time.timeScale = 2f;
+
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+            Time.timeScale = 3f;
+
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+            Time.timeScale = 4f;
+    }
+
+    #endregion //Methods
+}
