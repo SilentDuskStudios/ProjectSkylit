9c1589ec1d0e60829ad8f72a44d458c7dd8a7887
Jordan Labonne <SilentDuskStudios@hotmail.com>
2019-09-03 04:39:48 +1000

    Allowed the survivor to shoot their weapon.
    Create a raycast from the weapons muzzle.
    
    Set up the WeaponController to manage only firing the weapon for now.

---
+++ Project Skylit/Assets/Internal/Scripts/Controllers/WeaponController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    #region " - - - - - - Fields - - - - - - "
+
+    [SerializeField]
+    private Weapon weapon;
+
+    #endregion //Fields
+
+    #region " - - - - - - Methods - - - - - - "
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            weapon.Fire();
+    }
+
+    #endregion //Methods
+}
---
+++ Project Skylit/Assets/Internal/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    #region " - - - - - - Fields - - - - - - "
+
+    [SerializeField]
+    private Transform nozzle;
+
+    [SerializeField]
+    private float range;
+
+    #endregion //Fields
+
+    #region " - - - - - - Methods - - - - - - "
+
+    public void Fire()
+    {
+        Debug.DrawRay(nozzle.position, nozzle.forward * range, Color.red, 1f);
+        //How to do a real raycast...
+        
+    }
+
+    #endregion //Methods
+}
