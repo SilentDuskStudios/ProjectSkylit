7c497b3ed1a867cd3e988fd3c6c22a81774a9e51
Jordan Labonne <SilentDuskStudios@hotmail.com>
2019-10-04 20:21:45 +1000

    Airdrops self destruct after 45 seconds.
    To avoid clutter in the safe zone, all airdrops regardless of being opened or not will destroy themselves after 45 seconds from spawning.

---
+++ Project Skylit/Assets/Internal/Scripts/AirdropSelfDestruct.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class AirdropSelfDestruct : MonoBehaviour {
+
+    #region " - - - - - - Fields - - - - - - "
+
+    [SerializeField]
+    private float selfDestructTimer;
+
+    private float currentTimer;
+
+    [SerializeField]
+    private float descendSpeed;
+
+    private bool destroy;
+
+    #endregion
+
+    #region " - - - - - - Methods - - - - - - "
+
+    private void Update() {
+
+        currentTimer += Time.deltaTime;
+
+        if (currentTimer >= selfDestructTimer) {
+
+            destroy = true;
+            if (destroy) {
+                this.gameObject.GetComponent<BoxCollider>().enabled = false;
+                Destroy(this.gameObject, 3f);
+            }
+
+            this.gameObject.transform.Translate(Vector3.down * Time.deltaTime * descendSpeed);
+        }
+
+    }
+
+    #endregion
+
+}
